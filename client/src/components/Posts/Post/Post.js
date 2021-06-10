4e19265b2807f4069d1f2690c8c7e8444245bafa
Yomi Ojo <yomiojo001@gmail.com>
2021-06-10 15:04:46 +0000

    Vent init

---
+++ client/src/components/Posts/Post/Post.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import  {Card, CardActions, CardContent, CardMedia, Button, Typography} from '@material-ui/core';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import useStyles from './style'
+
+const Post = () => {
+
+    const classes = useStyles();
+
+    return (
+        <div>POST</div>
+    )
+}
+
+export default Post;
---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+const CONNECTION_URL = "mongodb+srv://yomsjs:Extension@100%25@nodecluster0.mrpzf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+.then(() => app.listen(PORT, () => console.log(`Server running on port:  ${PORT}`)))
+.catch((error) => console.log(error.message))
