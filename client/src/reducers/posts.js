86996ebeae07ea811f07d9434e426a17216d4a7f
Yomi Ojo <yomiojo001@gmail.com>
2021-07-05 22:35:55 +0100

    like post done

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -5,7 +5,8 @@ export default (posts = [], action) => {
         case 'DELETE':
             return posts.filter((post) => post._id !== action.payload )
         case 'UPDATE':
-            return posts.map((post) => post._id === action.payload._id ? action.payload : post)
+        case 'LIKEPOST':
+            return posts.map((post) => post._id === action.payload._id ? action.payload : post);
        case 'FETCH_ALL':
            return action.payload;
        case 'CREATE':
