0e51b8f7070ddadaf1a7be782262a3354ec82fda
Yomi Ojo <yomiojo001@gmail.com>
2021-07-12 15:53:43 +0000

    odebase restructure

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 const CONNECTION_URL = process.env.MONGO_URI
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
 .then(() => app.listen(PORT, () => console.log(`Server running on port:  ${PORT}`)))
